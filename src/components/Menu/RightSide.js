4e0e87d7e69dd415ebf878941da702a1967f01a5
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-03-10 15:39:18 -0300

    img/titulo/descrição

--- src/components/Menu/RightSide.js
+++ src/components/Menu/RightSide.js
@@ -6,10 +6,11 @@ function RightSide() {
         <div className="rightSide">
             <div className="header">
                 <img src="https://lh3.googleusercontent.com/zZpCsH2uswobVSd2L2OplA4ad59L2arVxYtsFpJSEc3hqYB5DeyhfosH8VO4hgOUxi9oJHsRNHFsUQEV98au4w" class="productImg"/>
-                <h2>Capacete de dima</h2>
+                <h4>Capacete de dima <p class="description">Descrição</p></h4>
             </div>
         </div>
     )
 }
+
 
 export default RightSide;
