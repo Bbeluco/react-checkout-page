777ccc803913826db3876ccb5526d858fc5226c8
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-03-10 16:00:40 -0300

    price

--- src/components/Menu/RightSide.js
+++ src/components/Menu/RightSide.js
@@ -6,8 +6,8 @@ function RightSide() {
         <div className="rightSide">
             <div className="header">
                 <img src="https://lh3.googleusercontent.com/zZpCsH2uswobVSd2L2OplA4ad59L2arVxYtsFpJSEc3hqYB5DeyhfosH8VO4hgOUxi9oJHsRNHFsUQEV98au4w" class="productImg"/>
-                <h4>Capacete de dima <p class="description">Descrição</p></h4>
-                <h4 class="price">$25.00</h4>
+                <h4 class="title">Capacete de dima <p class="description">Descrição</p></h4>
+                <h4 class="price">$25</h4>
             </div>
         </div>
     )
