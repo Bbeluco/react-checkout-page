afb1b213ee1710983f989ae6ef46321abe8e8410
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-03-10 14:51:16 -0300

    titulo e imagem alinhados

--- src/components/Menu/RightSide.js
+++ src/components/Menu/RightSide.js
@@ -3,9 +3,11 @@ import './RightSide.css'
 
 function RightSide() {
     return(
-        <div class="rightSide">
-            <img src="https://lh3.googleusercontent.com/zZpCsH2uswobVSd2L2OplA4ad59L2arVxYtsFpJSEc3hqYB5DeyhfosH8VO4hgOUxi9oJHsRNHFsUQEV98au4w" class="productImg"/>
-            <h1 class="productName">Testing text</h1>
+        <div className="rightSide">
+            <div className="header">
+                <img src="https://lh3.googleusercontent.com/zZpCsH2uswobVSd2L2OplA4ad59L2arVxYtsFpJSEc3hqYB5DeyhfosH8VO4hgOUxi9oJHsRNHFsUQEV98au4w" class="productImg"/>
+                <h2>Capacete de dima</h2>
+            </div>
         </div>
     )
 }
