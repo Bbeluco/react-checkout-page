0109ebc93ad31d96ac5a85b3aa351dcb42f48193
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-03-10 15:50:00 -0300

    checando o titulo

--- src/components/Menu/RightSide.js
+++ src/components/Menu/RightSide.js
@@ -7,6 +7,7 @@ function RightSide() {
             <div className="header">
                 <img src="https://lh3.googleusercontent.com/zZpCsH2uswobVSd2L2OplA4ad59L2arVxYtsFpJSEc3hqYB5DeyhfosH8VO4hgOUxi9oJHsRNHFsUQEV98au4w" class="productImg"/>
                 <h4>Capacete de dima <p class="description">Descrição</p></h4>
+                <h4 class="price">$25.00</h4>
             </div>
         </div>
     )
